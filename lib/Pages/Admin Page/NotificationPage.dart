5fe3761b37538a8e32e5110e0d646275917563e4
caocaa <rosaaulia4@gmail.com>
2023-05-19 10:23:39 +0700

    menambahkan isi notifikasi page admin

--- lib/Pages/Admin Page/NotificationPage.dart
+++ lib/Pages/Admin Page/NotificationPage.dart
@@ -69,6 +69,7 @@ class _NotificationPageState extends State<NotificationPage> {
                       child: Row(
                         children: [
                           Container(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
                             alignment: Alignment.center,
                             child: Icon(
                               Icons.warning_outlined,
@@ -77,9 +78,266 @@ class _NotificationPageState extends State<NotificationPage> {
                             ),
                           ),
                           Container(
-                            width: 300,
+                            width: 280,
                             child: Padding(
-                              padding: EdgeInsets.only(left: 15, top: 15),
+                              padding:
+                                  EdgeInsets.only(left: 10, top: 15, right: 15),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    "Segera Tindak Lanjuti!",
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'lato',
+                                    ),
+                                  ),
+                                  Expanded(
+                                      child: Padding(
+                                    padding: EdgeInsets.only(top: 6),
+                                    child: Text(
+                                      "Ada upaya masuk yang mencurigakan ke akun Anda dari perangkat baru pada 15 Maret 2023. Tinjau Sekarang.",
+                                      textAlign: TextAlign.justify,
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          fontFamily: 'lato',
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ))
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      padding: EdgeInsets.all(10),
+                    ),
+                  ),
+                  // isi notifikasi
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Container(
+                      width: 400,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              spreadRadius: 3,
+                              blurRadius: 10,
+                              offset: Offset(0, 3))
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: Row(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.only(right: 3),
+                                  child: const CircleAvatar(
+                                    radius: 20,
+                                    backgroundImage:
+                                        AssetImage('assets/image/user4.jpg'),
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.only(left: 3),
+                                  child: const CircleAvatar(
+                                    radius: 20,
+                                    backgroundImage:
+                                        AssetImage('assets/image/user5.png'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: 360,
+                            child: Expanded(
+                                child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: 20, bottom: 10, right: 20),
+                              child: Text(
+                                "Aida Sukitama, Gita M. Riyadhi, dan 115 pengguna lainnya mengirimkan permintaan unggah artikel. Lihat Sekarang.",
+                                textAlign: TextAlign.justify,
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    fontFamily: 'lato',
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // isi notifikasi
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 9),
+                    child: Container(
+                      width: 400,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              spreadRadius: 3,
+                              blurRadius: 10,
+                              offset: Offset(0, 3))
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 5),
+                            alignment: Alignment.center,
+                            child: Icon(
+                              Icons.report_outlined,
+                              size: 60,
+                              color: const Color(0xFF4F7D43),
+                            ),
+                          ),
+                          Container(
+                            width: 280,
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: 5, top: 25, right: 15, bottom: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    "Periksa Sekarang!",
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'lato',
+                                    ),
+                                  ),
+                                  Expanded(
+                                      child: Padding(
+                                    padding: EdgeInsets.only(top: 6),
+                                    child: Text(
+                                      "Ada sebanyak 250 laporan groker yang belum diperiksa. Lakukan pemerikasaan sekarang.",
+                                      textAlign: TextAlign.justify,
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          fontFamily: 'lato',
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ))
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      padding: EdgeInsets.all(10),
+                    ),
+                  ),
+                  // isi notifikasi
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Container(
+                      width: 400,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              spreadRadius: 3,
+                              blurRadius: 10,
+                              offset: Offset(0, 3))
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: Row(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.only(right: 3),
+                                  child: const CircleAvatar(
+                                    radius: 20,
+                                    backgroundImage:
+                                        AssetImage('assets/image/user3.jpg'),
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.only(left: 3),
+                                  child: const CircleAvatar(
+                                    radius: 20,
+                                    backgroundImage:
+                                        AssetImage('assets/image/user2.jpg'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: 360,
+                            child: Expanded(
+                                child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: 20, bottom: 10, right: 20),
+                              child: Text(
+                                "Howard, Lia Azzahra, dan 3 pengguna lainnya mengirimkan permintaan laporan pengguna. Periksa Sekarang. ",
+                                textAlign: TextAlign.justify,
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    fontFamily: 'lato',
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // isi notifikasi
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 9),
+                    child: Container(
+                      width: 400,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              spreadRadius: 3,
+                              blurRadius: 10,
+                              offset: Offset(0, 3))
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            alignment: Alignment.center,
+                            child: Icon(
+                              Icons.warning_outlined,
+                              size: 50,
+                              color: Colors.redAccent,
+                            ),
+                          ),
+                          Container(
+                            width: 280,
+                            child: Padding(
+                              padding:
+                                  EdgeInsets.only(left: 10, top: 15, right: 15),
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -115,7 +373,7 @@ class _NotificationPageState extends State<NotificationPage> {
                   ),
                   // isi notifikasi
                   Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    padding: const EdgeInsets.symmetric(vertical: 9),
                     child: Container(
                       width: 400,
                       height: 120,
@@ -132,24 +390,25 @@ class _NotificationPageState extends State<NotificationPage> {
                       child: Row(
                         children: [
                           Container(
-                            alignment: Alignment.topLeft,
-                            child: Image.asset(
-                              'assets/images/user4.jpg',
-                              height: 30,
-                              width: 20,
+                            padding: EdgeInsets.symmetric(horizontal: 5),
+                            alignment: Alignment.center,
+                            child: Icon(
+                              Icons.report_outlined,
+                              size: 60,
+                              color: const Color(0xFF4F7D43),
                             ),
                           ),
                           Container(
-                            alignment: AlignmentDirectional.centerStart,
-                            width: 300,
+                            width: 280,
                             child: Padding(
-                              padding: EdgeInsets.only(left: 15, top: 15),
+                              padding: EdgeInsets.only(
+                                  left: 5, top: 25, right: 15, bottom: 10),
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                 children: [
                                   Text(
-                                    "Segera Tindak Lanjuti!",
+                                    "Periksa Sekarang!",
                                     style: TextStyle(
                                       fontSize: 15,
                                       fontWeight: FontWeight.bold,
@@ -160,7 +419,7 @@ class _NotificationPageState extends State<NotificationPage> {
                                       child: Padding(
                                     padding: EdgeInsets.only(top: 6),
                                     child: Text(
-                                      "Ada upaya masuk yang mencurigakan ke akun Anda dari perangkat baru pada 20 Juni 2023. Tinjau Sekarang.",
+                                      "Ada sebanyak 57 laporan artikel yang belum diperiksa. Lakukan pemerikasaan sekarang.",
                                       textAlign: TextAlign.justify,
                                       style: TextStyle(
                                           fontSize: 12,
