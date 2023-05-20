73936e1308611d559643b0fadeee936f37360af4
nurandin <andininurul83@gmail.com>
2023-05-20 21:12:54 +0700

    penambahan SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:agrotalk/Pages/SplashScreenPage.dart';
 import 'package:flutter/material.dart';
 
 import 'Pages/loginPage.dart';
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Lato',
       ),
-      home: const LoginPage(),
+      home: const SplashScreenPage(),
     );
   }
 }
